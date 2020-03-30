1eeaddecbb38e9f5730d70befd12ec9420e0bac3
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2020-03-30 23:35:26 +0800

    'build'

---
+++ build/precache-manifest.572b5b980b11c654040dea390fec739c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6188bb20abb32b19c697737728eadca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "717a6a209eabe9a3909b",
+    "url": "/static/css/main.f8cdeb3e.chunk.css"
+  },
+  {
+    "revision": "44f9ef0ec35fdcb2a135",
+    "url": "/static/js/2.3a0776b2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3a0776b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717a6a209eabe9a3909b",
+    "url": "/static/js/main.2dc9a84e.chunk.js"
+  },
+  {
+    "revision": "24f922b9bf9effd28443",
+    "url": "/static/js/runtime-main.0bd40157.js"
+  }
+]);
